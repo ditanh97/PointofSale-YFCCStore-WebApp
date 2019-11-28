a4c91a0b368fa5750c27191c3520aa3cfa9ca957
ditutta <51126784+ditutta@users.noreply.github.com>
2019-11-28 17:24:43 +0700

    done learning flexbox

--- src/practice/flexbox/Flexbox.js
+++ src/practice/flexbox/Flexbox.js
@@ -4,18 +4,15 @@ import styles from './Flexbox.module.css'
 const Flexbox = () => {
     return(
         <div className={styles.flexContainer} >
-            <header className={styles.header}>
-                    4
-            </header>
-            <body>
-                <div>1</div>
-                <button className={styles.button}>Belut</button>
-                <button className={styles.button2}>Belut</button>
-                <button className={styles.button3}>Belut</button>
-                <div>2</div>
-                <div>3</div>
-
-            </body>
+            <div className={styles.sidebar}></div>
+            <div className={styles.fullContent}>
+                <div className={styles.header}>1</div>
+                <div className={styles.content}>
+                    <button className={styles.button}>Belut</button>
+                    <button className={styles.button2}>Belut</button>
+                    <button className={styles.button3}>Belut</button>
+                </div>
+            </div>
         </div>
     )
 }
